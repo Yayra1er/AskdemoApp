15464a8daecc62a3a81aa17b6710478e2fbba092
passuser122DHh <passmyall@gmail.com>
2024-07-01 12:52:59 +0200

    A propos and another

--- DemoApp/urls.py
+++ DemoApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('apropos/', views.apropos, name='apropos'),
 ]
--- DemoApp/views.py
+++ DemoApp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+
+def apropos(request):
+    return render(request, 'apropos.html')
